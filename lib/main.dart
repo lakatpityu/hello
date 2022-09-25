f061bb70619fec0f89067b27f996d6e6838aab1e
FL_Sabee <fapados77@gmail.com>
2022-09-25 11:11:40 +0000

    hello fds

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.fds
+  // This widget is the root of your.fds
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
